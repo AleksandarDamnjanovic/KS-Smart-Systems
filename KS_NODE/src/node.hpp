ecd5e4976bd5487552e1f601ce90f1cb9ff0da79
AleksandarDamnjanovic <aor.aleksandar@gmail.com>
2026-06-11 20:46:28 +0200

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   KS_NODE/src/node.hpp

--- KS_NODE/src/node.hpp
+++ KS_NODE/src/node.hpp
@@ -18,15 +18,15 @@
 #define NODE_SHUNT       2
 #define NODE_BRIDGE      3
 
-#define NODE_FINGERPRINT         "73 8A DF 2F 95 12 3C 8F 1D 88 D8 71 DD DC C5 8D DA 6C 4F 49"
-#define NODE_WLAN_SSID           "I'm_Iron_Man"
-#define NODE_WLAN_PASSWORD       "svarun444svarun"
+#define NODE_FINGERPRINT         ""
+#define NODE_WLAN_SSID           ""
+#define NODE_WLAN_PASSWORD       ""
 
-#define NODE_SERVER_ADDRESS      "192.168.0.24"
+#define NODE_SERVER_ADDRESS      ""
 #define NODE_SERVER_PORT         8883
 
-#define NODE_MQTT_USERNAME       "1f"
-#define NODE_MQTT_PASSWORD       "00004444"
+#define NODE_MQTT_USERNAME       ""
+#define NODE_MQTT_PASSWORD       ""
 
 class NODE{
     public:
